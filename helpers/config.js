0a9cd62cd5e96af365a7734d1448a4df8d0736a2
dereckdemezquita <dereck@demezquita.com>
2020-11-11 22:37:25 -0800

    Helpers with dummy data.

---
+++ helpers/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: [
+        "link-1.com",
+        "link-2.com"
+    ],
+    user_agent: "Chrome/85.0.4183.121 Safari/537.36"
+}
---
+++ helpers/scrape-video-links.js
@@ -0,0 +1,7 @@
+let links = document.querySelectorAll("a");
+
+for (let i = 0; i < links.length; i++) {
+    console.log(links[i].href)
+}
+
+document.querySelector("video").src;
